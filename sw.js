7853def947bf152ef49589ea6effefa87454d85c
Jiaolong <45839824+Wang-Jiaolong@users.noreply.github.com>
2021-04-20 08:56:26 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,23 +15,23 @@ const CACHE = CACHE_NAMESPACE + 'precache-then-runtime';
 const PRECACHE_LIST = [
   "./",
   "./offline.html",
-  "./js/jquery.min.js",
-  "./js/bootstrap.min.js",
-  "./js/hux-blog.min.js",
+  "./js/jquery.js",
+  "./js/bootstrap.js",
+  "./js/hux-blog.js",
   "./js/snackbar.js",
   "./img/icon_wechat.png",
   "./img/avatar-hux.jpg",
   "./img/home-bg.jpg",
   "./img/404-bg.jpg",
-  "./css/hux-blog.min.css",
-  "./css/bootstrap.min.css"
+  "./css/hux-blog.css",
+  "./css/bootstrap.css"
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css",
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/fonts/fontawesome-webfont.woff2?v=4.6.3",
   // "//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
+  "blog.jiaolong.space",
   "yanshuo.io",
   "cdnjs.cloudflare.com"
 ]
